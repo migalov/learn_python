5611851e1ad63245b51cf5a7295637cf96068ffc
Michel Muniagiseny <mafri.michel@gmail.com>
2024-02-05 08:27:06 +0300

    Start learn Django

---
+++ testchamber/news/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticlesForm
+
+# Create your views here.
+
+def news_home(request):
+  news = Articles.objects.order_by('title')
+  return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+  error = ''
+  if request.method == 'POST':
+    form = ArticlesForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+    else:
+      error = 'Форма была неверной'
+
+  form = ArticlesForm()
+
+  data = {
+    'form': form,
+    'error': error
+  }
+
+  return render(request, 'news/create.html', data)
---
+++ testchamber/testchamber/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testchamber project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testchamber.settings')
+
+application = get_wsgi_application()
